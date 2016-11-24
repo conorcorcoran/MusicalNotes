d6d28608ef08ccf72780f181bae94845eee863f5
20067633 <20067633@it103imac08.wit.ie>
2016-11-24 18:16:01 +0000

    Initial Commit

---
+++ MusicalNotes/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MusicalNotes
+//
+//  Created by 20067633 on 24/11/2016.
+//  Copyright © 2016 WIT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
